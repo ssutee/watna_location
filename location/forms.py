b3e6477ffde160884e55ad2f6572839b971ff906
Sutee Sudprasert <etipitaka@gmail.com>
2012-12-26 18:13:25 +0700

    update user

--- location/forms.py
+++ location/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
 from gmapi.forms.widgets import GoogleMap
@@ -12,6 +13,56 @@ from crispy_forms.bootstrap import FormActions
 
 import re
 
+class UserForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.layout = Layout(
+            Field('email'),
+            Field('first_name', css_class='input-xlarge'),
+            Field('last_name', css_class='input-xlarge'),
+            Field('old_password', css_class='input-xlarge'),
+            Field('new_password1', css_class='input-xlarge'),
+            Field('new_password2', css_class='input-xlarge'),
+            FormActions(
+                Submit('save', 'Save', css_class="btn btn-primary"),
+            )
+        )        
+        super(UserForm, self).__init__(*args, **kwargs) 
+
+    email = forms.CharField(label=_('E-mail'), max_length=50, 
+        widget=forms.HiddenInput())
+            
+    first_name = forms.CharField(label=_('First name'), max_length=50)
+    last_name = forms.CharField(label=_('Last name'), max_length=50)
+    
+    old_password = forms.CharField(required=False,
+        label=_('Password'), widget=forms.PasswordInput()
+    )
+    
+    new_password1 = forms.CharField(required=False,
+        label=_('New password'), widget=forms.PasswordInput()
+    )
+
+    new_password2 = forms.CharField(required=False,
+        label=_('New password (again)'), widget=forms.PasswordInput()
+    )
+    
+    def clean_old_password(self):
+        email = self.cleaned_data['email']
+        password = self.cleaned_data['old_password']
+        if authenticate(email=email, password=password):
+            return password
+        raise forms.ValidationError(_('Wrong password'))
+        
+    def clean_new_password2(self):
+        if 'new_password1' in self.cleaned_data or 'new_password2' in self.cleaned_data:
+            password1 = self.cleaned_data['new_password1']
+            password2 = self.cleaned_data['new_password2']
+            if password1 == password2:
+                return password2
+            raise forms.ValidationError(_('Password do not match'))        
+        
 class RegistrationForm(forms.Form):
     
     def __init__(self, *args, **kwargs):
@@ -27,7 +78,6 @@ class RegistrationForm(forms.Form):
                 Submit('register', 'Register', css_class="btn btn-primary"),
             )
         )
-        
         super(RegistrationForm, self).__init__(*args, **kwargs)
         
     first_name = forms.CharField(label=_('First name'), max_length=50)
@@ -46,18 +96,15 @@ class RegistrationForm(forms.Form):
             password2 = self.cleaned_data['password2']
             if password1 == password2:
                 return password2
-            raise forms.ValidationError(_('Password do not match.'))
+            raise forms.ValidationError(_('Password do not match'))
             
-    def clean_username(self):
-        username = self.cleaned_data['username']
-        if not re.search(r'^\w+$', username):
-            raise forms.ValidationError(_('Username can only contain '
-                'alphanumeric characters and the underscore.'))
+    def clean_email(self):
+        email = self.cleaned_data['email']
         try:
-            User.objects.get(username=username)
+            User.objects.get(email=email)
         except User.DoesNotExist:
-            return username
-        raise forms.ValidationError(_('Username is already taken.'))
+            return email
+        raise forms.ValidationError(_('E-mail is already taken.'))
         
 class MapForm(forms.Form):
     gmap = forms.Field(widget=GoogleMap(attrs={'width':1024, 'height':600, 'nojquery':True}))
@@ -101,4 +148,3 @@ class LocationForm(ModelForm):
         queryset=Activity.objects.all(), 
         widget=forms.widgets.CheckboxSelectMultiple()
     )
-        
--- location/views.py
+++ location/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import simplejson
 
-from location.forms import MapForm, SearchMapForm, LocationForm, RegistrationForm
+from location.forms import MapForm, SearchMapForm, LocationForm, RegistrationForm, UserForm
 from location.models import Location
 
 from gmapi import maps
@@ -17,10 +17,13 @@ def create_info_content(location):
     for activity in location.activities.all():
         activities += '<li>'+activity.name+'</li>'
     activities += '</ul>'    
-    return '<div style="height:130px; width:300px;">%s</br>%s %s</br>%s, %s%s</div>' % (location.place_name, location.user.first_name, 
-        location.user.last_name, location.phone_number, location.user.email, activities)
+    return '<div style="height:130px; width:300px;">%s</br>%s %s</br>%s, %s%s</div>' % (
+        location.place_name, location.user.first_name, 
+        location.user.last_name, location.phone_number, 
+        location.user.email, activities)
 
 def map_page(request):
+    request.session['next'] = '/'
     gmap = maps.Map(
         opts = {
             'center': maps.LatLng(14.01012, 100.82302),
@@ -77,13 +80,38 @@ def create_search_map_form():
         
     return SearchMapForm(initial={'gmap': gmap})
     
-
+@login_required
 def locations_page(request):
+    request.session['next'] = '/places'
     context = {'active_menu':1, 'locations': Location.objects.filter(user=request.user).all()}
     if 'message' in request.flash and request.flash['message']:
         context['alert_type'], context['alert_message'] = request.flash['message']
     return render(request, 'locations_page.html', context)
 
+@login_required
+def edit_user_page(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST);
+        if form.is_valid():
+            password = form.cleaned_data['new_password1']
+            if len(password) > 0:
+                request.user.set_password(password)
+            request.user.first_name = form.cleaned_data['first_name'];
+            request.user.last_name = form.cleaned_data['last_name'];
+            request.user.save()            
+            request.flash['message'] = ('alert-success', _('User updated successfully'))
+            return HttpResponseRedirect(request.session.get('next', '/'))                  
+    else:        
+        form = UserForm(initial={
+            'email':request.user.email,
+            'first_name':request.user.first_name,
+            'last_name':request.user.last_name})
+
+    context = {'form': form}
+    return render(request, 'edit_user_page.html', context)
+
+
+@login_required
 def edit_location_page(request, pk):
     location = Location.objects.get(pk=int(pk))    
     if request.method == 'POST':
@@ -100,7 +128,7 @@ def edit_location_page(request, pk):
     context = {'form': form, 'map_form': create_search_map_form()}
     return render(request, 'edit_location_page.html', context)
     
-
+@login_required
 def add_location_page(request):
     if request.method == 'POST':
         form = LocationForm(request.POST)
