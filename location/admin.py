5a7f9572fec1a31079751844127ad795bf92d5bf
Sutee Sudprasert <etipitaka@gmail.com>
2012-12-28 16:05:58 +0700

    update

--- location/admin.py
+++ location/admin.py
@@ -5,8 +5,10 @@ from location.widgets import JSColorColorPicker
 class ActivityAdmin(admin.ModelAdmin):
     formfield_overrides = {
         ColorModelField: {'widget': JSColorColorPicker }
-    }    
-
+    }
+        
+    list_display = ('name', 'priority',)
+    
 admin.site.register(Location)
 admin.site.register(Status)
 admin.site.register(Activity, ActivityAdmin)
