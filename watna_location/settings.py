448479a40f123fb81d433326df379a92881f4fbd
Sutee Sudprasert <etipitaka@gmail.com>
2012-12-31 00:26:25 +0700

    save click state and add country name into info modal

--- watna_location/settings.py
+++ watna_location/settings.py
@@ -162,8 +162,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'location.context_processors.profile',
 )
 
-
-
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
