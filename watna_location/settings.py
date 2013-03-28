82e73f61fb7ba45ce55c2ca07f3b90ec567c34d5
Sutee Sudprasert <etipitaka@gmail.com>
2013-03-28 22:39:37 +0700

    change db user from root to sutee

--- watna_location/settings.py
+++ watna_location/settings.py
@@ -37,7 +37,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'watna_location',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
+        'USER': 'sutee',                      # Not used with sqlite3.
         'PASSWORD': 'g2-94b',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
